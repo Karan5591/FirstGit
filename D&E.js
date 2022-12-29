19bdb7c999051feea37824a2340334fbd8618c73
Karan5591 <karansingh5591>
2022-12-30 00:08:54 +0530

    Filter added

--- D&E.js
+++ D&E.js
@@ -7,13 +7,28 @@ form.addEventListener('submit', addItem);
 //delete event
 itemList.addEventListener('click', removeItem)
 
+//Add Filter
+filter.addEventListener('keyup', filterItems);
+
+const textItem=document.createElement('INPUT');
+textItem.setAttribute("type","text");
+textItem.setAttribute("class","form-control mr-2");
+textItem.setAttribute("id","item");
+let parentEle = document.getElementById('addForm');
+const list=document.getElementById('addForm')
+list.insertBefore(textItem,list.children[1]);
+
 //Add item
 function addItem(e)
 {
     e.preventDefault();
     //console.log(1);
-    //get inpur value
-    var newItem=document.getElementById('item').value;
+//Create a new text field
+
+
+
+    //get input value
+    var newItem=document.getElementsByClassName('form-control mr-2');
 
     //Create new li element
     var li=document.createElement('li');
@@ -22,9 +37,14 @@ function addItem(e)
     li.className='list-group-item';
     
     //Add text with input value
-    li.appendChild(document.createTextNode(newItem));
+    
+        li.appendChild(document.createTextNode(newItem[0].value+" "+newItem[1].value));
+//        li.appendChild(document.createTextNode(newItem[1].value));
+    
+        
+       
 
-    //create edit button
+   //create edit button
     var editBtn=document.createElement('button');
 
     //add classses to del button
@@ -71,6 +91,35 @@ function removeItem(e)
         itemList.removeChild(li);
        }
     }
+}
+
+//Filter Items
+function filterItems(e)
+{
+    //convert text to lowecase
+
+    var text= e.target.value.toLowerCase();
+    //get li
+    var items=itemList.getElementsByTagName('li');
+
+    //convert to an array
+    Array.from(items).forEach(function(item)
+    {
+        var itemName=item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1)
+        {
+            item.style.display='block';
+
+        }
+        else{
+            item.style.display='none';
+        }
+
+    });
+    
+    
+
+     
 }
 
 
