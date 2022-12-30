894e30ddea04616f368d87450363919555001811
Karan5591 <karansingh5591>
2022-12-30 12:10:12 +0530

    local storage using object

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 const btn=document.querySelector('.btn');
 
-btn.addEventListener('click', e => {
+/*btn.addEventListener('click', e => {
     e.preventDefault();
     console.log(e.target.className);
     var name=document.getElementById('name').value;
@@ -9,4 +9,17 @@ btn.addEventListener('click', e => {
     localStorage.setItem('name',name);
     localStorage.setItem('Email', mail);
     
+  }); */
+
+  //Storing data as an object
+
+  btn.addEventListener('click', e =>{
+    e.preventDefault();
+    let myobj={
+        name: document.getElementById('name').value,
+        mail: document.getElementById('email').value
+    }
+    myobj_serialized=JSON.stringify(myobj);
+
+    localStorage.setItem('myobj', myobj_serialized);
   });
