6e7201f23b4e8630324df64076c0d197d49a1d6f
Karan5591 <karansingh5591>
2023-01-10 11:58:54 +0530

    Async await

--- PromisesAllusingAsyncAwait.js
+++ PromisesAllusingAsyncAwait.js
@@ -24,33 +24,34 @@ function createPost(post)
     
     console.log("Before creating this post time is:-"+new Date());
     return new Promise((resolve, reject)=>{
-     posts.push(post);
-      resolve("Post created on:-");
-    });
-            
+    setTimeout(()=>{
+        posts.push(post);
+        console.log("Element inserted at:-"+new Date());
+        resolve("");
+    },2000)
+    
+});
 }
+        
+
 function deletePost()
 {
     return new Promise((resolve, reject)=>{
-        console.log("Removing element.")
-        posts.pop();
-     resolve("Element removed on:-");
-    });
-}
-let updateLastUserActivityTime=new Promise((resolve, reject)=>{
-                
+
+   
     setTimeout(()=>{
-        
-        resolve(new Date());
-    }, 1000)
+        posts.pop();
+        console.log("Element removed at:-"+new Date());
+        resolve("");
+    },2000)
     
-});
-const post1=async ()=>{
-let create= await createPost({title:'Post Three'}).then((value)=>console.log(value));
-let activity=await updateLastUserActivityTime.then((value)=>console.log(value))
-let get1=await getPosts().then((value)=>console.log(value));
-let del=  deletePost().then((value)=>console.log(value));
-let activity1=await updateLastUserActivityTime.then((value)=>console.log(value))
-let get2=await getPosts().then((value)=>console.log(value));
+}) ; 
+     
+}
+async function post1(){
+await createPost({title:'Post Three'});
+await getPosts();
+await deletePost();
+await getPosts();
 }
 post1();
